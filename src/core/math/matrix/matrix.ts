77a67642ff3b6a918d4b9310c2d3aca95a8b66be
Gabriel Medeiros Souza <gabriel@wood>
2023-04-04 19:02:59 -0300

    feature add string visualization to Matrices

--- src/core/math/matrix/matrix.ts
+++ src/core/math/matrix/matrix.ts
@@ -6,6 +6,21 @@ export abstract class Matrix<T extends Matrix<T>>
 
   public abstract Clone(): T
 
+  public ToString(): string
+  {
+  	let message = "";
+
+  	for (let i = 0; i < this.data.length; i++)
+  	{
+  		const value = this.data[i];
+    	message += value < 0 ? value.toFixed(2) + " " : " " + value.toFixed(2) + " ";
+
+    	if ((i + 1) % this.stride === 0) message += "\n";
+  	}
+
+  	return message;
+  }
+
   public *[Symbol.iterator](): Generator<number>
   {
   	for (let i = 0; i <= this.data.length; i++)
