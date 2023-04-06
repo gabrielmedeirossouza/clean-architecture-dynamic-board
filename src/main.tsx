4a262ef3df9bd5b65b38c0137dcaa4e956a13877
Gabriel Medeiros Souza <gabriel@wood>
2023-04-05 22:01:21 -0300

    feature add react

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
+	<React.StrictMode>
+		<React.Fragment>hello world</React.Fragment>
+	</React.StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
-import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+	plugins: [react()],
 	resolve: {
 		alias: [
 			{ find: "@", replacement: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src") },
